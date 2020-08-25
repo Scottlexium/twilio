9b1b2e0bdf5480ec8899336056a18715f1c84047
Scottlexium <54009900+Scottlexium@users.noreply.github.com>
2020-08-25 16:39:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from twilio.twiml.voice_receive import VoiceResponse
+
+
+app = Flask(_name_)
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice();
+resp = VoiceResponse()
+
+resp.say("Hello From Scott Lexium!")
+return str(resp)
+
+if _name_ == "_main_":
+    app.run(debug=True)
